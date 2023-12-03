7099e7ecc5df8968aec9ff6ee4e78e1765433809
Hamidali <ali.hamid161999@gmail.com>
2023-12-03 10:09:12 +0530

    add

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -50,7 +50,8 @@ const about = () => {
     <>
       <main className="flex w-full flex-col items-center justify-center dark:text-light">
         <Layout className="pt-6">
-        <AnimatedText text="Md Hamid Ali !" className="mb-16 lg:&apos;!text-7xl sm:&apos;!text-6xl xs:&apos;!text-4xl sm:mb-8"/>
+        {/* hgghhghgh */}
+        {/* <AnimatedText text="Md Hamid Ali !" className="mb-16 lg:&apos;!text-7xl sm:&apos;!text-6xl xs:&apos;!text-4xl sm:mb-8"/> */}
           <div className=" grid w-full grid-cols-8 gap-16 sm:gap-8 ">
             <div className=" col-span-3 flex flex-col items-start justify-center xl:col-span-4 md:order-2 md:col-span-8">
               <h2 className=" mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light">
