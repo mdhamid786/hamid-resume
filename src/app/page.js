25e3af1aa07cb4f4e732d2911154a4e5f30998a1
Hamidali <ali.hamid161999@gmail.com>
2023-12-31 12:44:20 +0530

    add

--- src/app/page.js
+++ src/app/page.js
@@ -50,7 +50,7 @@ export default function Home() {
               </p>
 
               <div className=" flex items-center self-start mt-2 lg:self-center">
-                {/* <Link
+                <Link
                   className="flex items-center bg-dark text-light  p-2.5 px-6 rounded-lg text-lg
                    font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent
                     hover:border-dark dark:text-dark dark:bg-light hover:dark:bg-dark hover:dark:text-light hover:dark:border-light
@@ -62,11 +62,7 @@ export default function Home() {
                 >
                   Resume
                   <LinkArrow className=" w-6 ml-1" />
-                </Link> */ <Link href="/resume.pdf" passHref>
-        <a target="_blank" rel="noopener noreferrer">
-          Download PDF
-        </a>
-      </Link>}
+                </Link>
                 <Link
                   href="mailto:ali.hamid161999@gmail.com"
                   target="_blank"
